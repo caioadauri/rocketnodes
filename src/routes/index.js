be700fa2f44dede7f8e513c218478750ebc9a760
caioadauri <caioadaurigomes@gmail.com>
2025-03-19 07:27:53 -0300

    add token in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express')
 
 const userRouter = require('./users.routes')
+const sessionsRouter = require('./sessions.routes')
 const notesRouter = require('./notes.routes')
 const tagsRouter = require('./tags.routes')
 
 const routes = Router()
 
 routes.use('/users', userRouter)
+routes.use('/sessions', sessionsRouter)
 routes.use('/notes', notesRouter)
 routes.use('/tags', tagsRouter)
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 
 const UserController = require('../controllers/UserController.js')
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated.js")
 
 const userRoutes = Router()
 
 const userController = new UserController()
 
 userRoutes.post('/', userController.create)
-userRoutes.put('/:id', userController.update)
+userRoutes.put('/', ensureAuthenticated, userController.update)
 
 module.exports = userRoutes
